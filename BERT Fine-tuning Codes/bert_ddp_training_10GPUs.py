2cab4e60a9c0c9287c2946f6f190d9cb9e2a046f
Abrar Alam, PhD <abrara@smu.edu>
2026-05-07 15:02:16 -0500

    Updated comments for consistency and clarity

--- BERT Fine-tuning Codes/bert_ddp_training_10GPUs.py
+++ BERT Fine-tuning Codes/bert_ddp_training_10GPUs.py
@@ -176,7 +176,7 @@ def main_worker(rank, world_size, local_rank, epochs = 10, batch_size = 128, lr 
 		save_model(model, 'bert_ddp_trained_model_10GPUs')
 	clean_process()
 
-# Saving the model
+# Saving the fine-tuned model checkpoints
 def save_model(model, save_path):
 	os.makedirs(save_path, exist_ok = True)
 	model_to_save = model.module if hasattr(model, 'module') else model
